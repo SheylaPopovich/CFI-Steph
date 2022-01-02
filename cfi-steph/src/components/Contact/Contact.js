77a97e2959a02a8586e07c78a21afdd5bff5395a
Sheyla <Sheylapopovich@gmail.com>
2022-01-02 16:49:34 -0700

    install react bootstrap

--- cfi-steph/src/components/Contact/Contact.js
+++ cfi-steph/src/components/Contact/Contact.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import { Form } from "react-bootstrap";
 
 function Contact() {
     return (
-        <div>
-            <h1>TESTING FOR CONTACT PAGE</h1>
-        </div>
+        <form
+        action="https://formsubmit.co/stephanielpopovich@gmail.com"
+        method="POST"
+        class="shadow p-3 mb-3 bg-body rounded align-self-center w-50"
+      >
+        <Form.Group
+          className="mb-3 justify-content-center "
+          controlId="exampleForm.ControlInput1"
+        >
+          <h1 class="formTitles pb-2">Contact Me</h1>
+          <div class="row mb-3">
+            <Form.Label className="formTitles col-md-2">Name: </Form.Label>
+            <div class="col-md-10">
+              <input type="text" name="name" required class="pb-2" />
+            </div>
+          </div>
+          <div class="row mb-3">
+            <Form.Label className="formTitles col-md-2">Email: </Form.Label>
+            <div class="col-md-10">
+              <input type="email" name="email" required />
+            </div>
+          </div>
+        </Form.Group>
+        <Form.Group className="row mb-3 pe-4 ps-4">
+          <textarea
+            name="textarea"
+            placeholder="Message me here"
+            class="mb-2 "
+          ></textarea>
+          <button type="submit" class="btn btn-primary">
+            Send
+          </button>
+        </Form.Group>
+      </form>
     )
 }
 
