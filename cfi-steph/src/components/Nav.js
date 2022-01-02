2ab45ef40120ce522f17bf5d67f81b72fe472bfc
Sheyla <Sheylapopovich@gmail.com>
2022-01-02 13:32:31 -0700

    changed navbar education group lists

--- cfi-steph/src/components/Nav.js
+++ cfi-steph/src/components/Nav.js
@@ -1,70 +1,55 @@
-import React from 'react'
+import React from "react";
 
 function Nav() {
-    return (
-        <div>
-              <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
-        <img id="logo" src="./assets/images/CFI_Steph_logo.png" />
-      </a>
+  return (
+    <div>
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="https://bulma.io">
+            <img id="logo" src="./assets/images/CFI_Steph_logo.png" />
+          </a>
 
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample"
-       >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
+          <a
+            role="button"
+            class="navbar-burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
 
-    <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-start">
-        <a class="navbar-item">
-          Home
-        </a>
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item">Home</a>
 
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a href="./education.html" class="navbar-link">
-            Education
-          </a>
-          
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a href="./education.html" class="navbar-link">
+                Education
+              </a>
 
-          <a class="navbar-item">
-            About
-          </a>
-          <a class="navbar-item">
-            Shop
-          </a>
-          <a href="./contact.html" class="navbar-item">
-            Contact
-          </a>
-          <a class="navbar-item">
-            Blog
-          </a>
-          <div class="navbar-dropdown">
-            <a class="navbar-item">
-              Basic Education
-            </a>
-            <a class="navbar-item">
-              Advanced Education
-            </a>
-            <a class="navbar-item">
-              Steph's Student
-            </a>
-            <a class="navbar-item">
-              Suggested Reading
-            </a>
-         
-            <a class="navbar-item">
-              Report an issue
-            </a>
+              <a class="navbar-item">About</a>
+              <a class="navbar-item">Shop</a>
+              <a href="./contact.html" class="navbar-item">
+                Contact
+              </a>
+              <a class="navbar-item">Blog</a>
+              <div class="navbar-dropdown">
+                <a class="navbar-item">Private Pilot Knowledge</a>
+                <a class="navbar-item">Instrument Rating Knowledge</a>
+                <a class="navbar-item">Commercial Pilot Knowledge</a>
+                <a class="navbar-item">Multi-Engine Knowledge</a>
+                <a class="navbar-item">Suggested Reading</a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </nav>
     </div>
-  </nav>
-        </div>
-    )
+  );
 }
 
-export default Nav
+export default Nav;
