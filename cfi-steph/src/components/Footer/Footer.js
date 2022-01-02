e80f5a6cae2c7b010a9d635908466c611a104a65
Sheyla <Sheylapopovich@gmail.com>
2022-01-02 16:35:39 -0700

    created a footer component

---
+++ cfi-steph/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <h5>Disclaimer</h5>
+            <p>While attempts have been made to verify the accuracy of the information on this website, the author does not assume any responsibilities of liabilities for errors contained on this site or for misuse of any information. You may only use this information at you own risk.</p>
+        </div>
+    )
+}
+
+export default Footer
--- cfi-steph/src/components/HomePage/Homepage.js
+++ cfi-steph/src/components/HomePage/Homepage.js
@@ -3,7 +3,8 @@ import React from 'react'
 function Homepage() {
     return (
         <div>
-            <h1>TESTING FOR HOME PAGE INSERT WELCOME GREETING HERE</h1>
+            <h1>Welcome</h1>
+            <p>This site is still very new and I'm going to slowly be adding new information as I have time. So continue to come back for new stuff and new blog posts, where I will tackle discussion on specific topics (let me know there are any topics you’d like discussed). All information on this page is free of charge to anyone who chooses to use it. However, if you have the ability to help keep this site running, you will find a link below to donate. All donations go towards the fees associated with running this website. Thank you in advance, your contributions are appreciated; as without contributions, the site may eventually have to shut down.</p>
         </div>
     )
 }
