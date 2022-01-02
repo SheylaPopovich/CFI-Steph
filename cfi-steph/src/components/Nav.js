b48c30599b018e5e19f019e287e8f5c178e6e426
Sheyla <Sheylapopovich@gmail.com>
2022-01-02 14:08:45 -0700

    hamburger navbar working successfully

--- cfi-steph/src/components/Nav.js
+++ cfi-steph/src/components/Nav.js
@@ -1,17 +1,21 @@
 import React from "react";
 
 function Nav() {
+  const [isActive, setisActive] = React.useState(false);
   return (
     <div>
-      <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="https://bulma.io">
-            <img id="logo" src="./assets/images/CFI_Steph_logo.png" />
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            {/* <img id="logo" src="./assets/images/CFI_Steph_logo.png" /> */}
           </a>
 
           <a
+            onClick={() => {
+              setisActive(!isActive);
+            }}
             role="button"
-            class="navbar-burger"
+            className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
             aria-label="menu"
             aria-expanded="false"
             data-target="navbarBasicExample"
@@ -22,28 +26,31 @@ function Nav() {
           </a>
         </div>
 
-        <div id="navbarBasicExample" class="navbar-menu">
-          <div class="navbar-start">
-            <a class="navbar-item">Home</a>
+        <div
+          id="navbarBasicExample"
+          className={`navbar-menu ${isActive ? "is-active" : ""}`}
+        >
+          <div className="navbar-start">
+            <a className="navbar-item">Home</a>
 
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a href="./education.html" class="navbar-link">
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a href="" class="navbar-link">
                 Education
               </a>
+              <div className="navbar-dropdown">
+                <a className="navbar-item">Private Pilot Knowledge</a>
+                <a className="navbar-item">Instrument Rating Knowledge</a>
+                <a className="navbar-item">Commercial Pilot Knowledge</a>
+                <a className="navbar-item">Multi-Engine Knowledge</a>
+                <a className="navbar-item">Suggested Reading</a>
+              </div>
 
-              <a class="navbar-item">About</a>
-              <a class="navbar-item">Shop</a>
-              <a href="./contact.html" class="navbar-item">
+              <a className="navbar-item">About</a>
+              <a className="navbar-item">Shop</a>
+              <a href="" className="navbar-item">
                 Contact
               </a>
-              <a class="navbar-item">Blog</a>
-              <div class="navbar-dropdown">
-                <a class="navbar-item">Private Pilot Knowledge</a>
-                <a class="navbar-item">Instrument Rating Knowledge</a>
-                <a class="navbar-item">Commercial Pilot Knowledge</a>
-                <a class="navbar-item">Multi-Engine Knowledge</a>
-                <a class="navbar-item">Suggested Reading</a>
-              </div>
+              <a className="navbar-item">Blog</a>
             </div>
           </div>
         </div>
