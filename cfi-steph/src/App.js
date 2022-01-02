7becc2bf4007b0273ea3f5888abcfa19ad341640
Sheyla <Sheylapopovich@gmail.com>
2022-01-02 16:39:59 -0700

    added content on about component

--- cfi-steph/src/App.js
+++ cfi-steph/src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
 import Contact from "./components/Contact/Contact";
 import Nav from "./components/NavBar/Nav";
-
+import Footer from "./components/Footer/Footer";
 
 function App() {
-
   return (
     <div className="App">
       <Nav />
-    
+      <Footer />
     </div>
   );
 }
--- cfi-steph/src/components/About/About.js
+++ cfi-steph/src/components/About/About.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
 
 function About() {
-    return (
-        <div>
-            <h1>TESTING ABOUT PAGE</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>TESTING ABOUT PAGE</h1>
+      <p>
+        Fun fact, I was a flight attendant that became a pilot. I went from
+        serving drinks on a plane, to manipulating the aircraft controls. I had
+        no aviation knowledge, other than the basics I knew from being a flight
+        attendant. One day I decided to finally take the leap and jump head
+        first. I quit my job and started flight training right away. I learned
+        very quickly that it was going to be a steep learning curve. I was able
+        to do a very fast paced program and spent 10 months of my life doing
+        nothing but eat, sleep, fly, and study aviation. I’m now fleet technical
+        instructor teaching 737 systems knowledge. This website came about
+        because I struggled to understand a majority of the information and
+        let’s face it, not a lot of ground was given. I had to find other
+        sources to get information broken down. Once becoming a CFI, I decided I
+        wanted to create a website that help take all the concepts in flight
+        training and break them down into a language much easier to understand.
+        The website is a project that I’ve been working on when I can find time
+        in between flying and my continued education towards my bachelors
+        degree. I encourage you to return often and explore for more updates and
+        topics discussed. I also urge you to subscribe and get email updates
+        when new discussions are posted.{" "}
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
