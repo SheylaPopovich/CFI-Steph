43c3db620fb31fd55538ec3e8bcf38c7c3152cf7
Sheyla <Sheylapopovich@gmail.com>
2022-01-03 16:24:13 -0700

    created a main.js component for routes path on app.js file

--- cfi-steph/src/App.js
+++ cfi-steph/src/App.js
@@ -1,17 +1,29 @@
 import "./App.css";
-import Nav from "./components/NavBar/Nav";
-import Footer from "./components/Footer/Footer";
+// import Nav from "./components/NavBar/Nav";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Main from "./pages/Main";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
       
+
+    <Router>
+        <>
+          <Routes>
+            <Route exact path="/" element={ <Main /> }></Route>
+           
+          </Routes>
+        </>
+      </Router>
     
-      <Nav />
+      {/* <Nav /> */}
      
     </div>
+
+
   );
 }
 
--- cfi-steph/src/pages/Main.js
+++ cfi-steph/src/pages/Main.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-function LandingPage() {
+function Main() {
     return (
         <div>
             <h1>LANDING PAGE</h1>
+            
         </div>
     )
 }
 
-export default LandingPage
+export default Main
 
 
