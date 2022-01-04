a6041b04365353f5575850494f24ac2978b500cd
Sheyla <Sheylapopovich@gmail.com>
2022-01-03 19:35:01 -0700

    attempting to assign homepage component to onclick feature

--- cfi-steph/src/pages/Main.js
+++ cfi-steph/src/pages/Main.js
@@ -1,34 +1,36 @@
 import React, { useState } from "react";
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 import HomePage from "../components/HomePage/Homepage";
 
 
 function Main() {
-    const [value, setValue] = useState("");
+  const [value, setValue] = useState("");
 
-    const handleChange = e => {
-      setValue(e.target.value);
-    };
-  
-    const handleSubmit = e => {
-      e.preventDefault();
-      alert("you have searched for - " + value);
-      // or you can send data to backend
-    };
-  
-    const handleKeypress = e => {
-        //it triggers by pressing the enter key
-      if (e.keyCode === 13) {
-        handleSubmit();
-      }
-    };
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
-    return (
-        // <div>
-        //     <h1>LANDING PAGE</h1>
-            
-        // </div>
-        <div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("you have searched for - " + value);
+    // or you can send data to backend
+  };
+
+  const handleKeypress = (e) => {
+    //it triggers by pressing the enter key
+    if (e.keyCode === 13) {
+      handleSubmit();
+    }
+  };
+
+  
+
+  return (
+    // <div>
+    //     <h1>LANDING PAGE</h1>
+
+    // </div>
+    <div>
       <form>
         <input
           value={value}
@@ -38,12 +40,12 @@ function Main() {
         <button onClick={handleSubmit} type="submit">
           Submit
         </button>
-        <Button variant="btn btn-success" onClick={() => ('/Products') }>Click button to view products</Button>
+        <Button variant="btn btn-success" onClick={() => <HomePage />}>
+          Click button to view products
+        </Button>
       </form>
     </div>
-    )
+  );
 }
 
-export default Main
-
-
+export default Main;
