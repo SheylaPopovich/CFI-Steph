53a3a793f6d292c0d6d266fe0d976c0a957645e8
Sheyla <Sheylapopovich@gmail.com>
2022-01-03 20:36:19 -0700

    removed handlesubmit on Main.js

--- cfi-steph/src/App.js
+++ cfi-steph/src/App.js
@@ -19,7 +19,7 @@ function App() {
         </>
       </Router>
     
-      <Nav />
+      {/* <Nav /> */}
      
     </div>
 
--- cfi-steph/src/pages/Main.js
+++ cfi-steph/src/pages/Main.js
@@ -1,15 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { Button } from "react-bootstrap";
 import Homepage from "../components/HomePage/Homepage";
 
 function Main() {
-  const [value, setValue] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert("you have searched for - " + value);
-    // or you can send data to backend
-  };
+ 
 
   return (
     <Button variant="btn btn-success" onClick={Homepage} href="#homepage">
