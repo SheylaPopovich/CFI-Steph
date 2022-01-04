52903e16a05289bcf868e305051949fc7c8dee12
Sheyla <Sheylapopovich@gmail.com>
2022-01-03 21:01:51 -0700

    imported useHistory from react dom

--- cfi-steph/src/pages/Main.js
+++ cfi-steph/src/pages/Main.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import Homepage from "../components/HomePage/Homepage";
+import { useHistory } from 'react-router-dom';
 
 function Main() {
- 
-
   return (
-    <Button variant="btn btn-success" onClick={Homepage} href="#homepage">
+    <Button variant="btn btn-success" onClick="/homepage" href="/homepage">
       Click button to view products
     </Button>
   );
