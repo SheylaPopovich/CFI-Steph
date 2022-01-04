b8a9f4a1a678e164da36717159302fa89c65d6f8
Sheyla <Sheylapopovich@gmail.com>
2022-01-03 20:19:34 -0700

    removed form elements on main.js, keeping onclick button only

--- cfi-steph/src/pages/Main.js
+++ cfi-steph/src/pages/Main.js
@@ -1,14 +1,9 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import HomePage from "../components/HomePage/Homepage";
-
+import Homepage from "../components/HomePage/Homepage";
 
 function Main() {
   const [value, setValue] = useState("");
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,35 +11,10 @@ function Main() {
     // or you can send data to backend
   };
 
-  const handleKeypress = (e) => {
-    //it triggers by pressing the enter key
-    if (e.keyCode === 13) {
-      handleSubmit();
-    }
-  };
-
-  
-
   return (
-    // <div>
-    //     <h1>LANDING PAGE</h1>
-
-    // </div>
-    <div>
-      <form>
-        <input
-          value={value}
-          onChange={handleChange}
-          onKeyPress={handleKeypress}
-        />
-        <button onClick={handleSubmit} type="submit">
-          Submit
-        </button>
-        <Button variant="btn btn-success" onClick={() => <HomePage />}>
-          Click button to view products
-        </Button>
-      </form>
-    </div>
+    <Button variant="btn btn-success" onClick={Homepage} href="/homepage">
+      Click button to view products
+    </Button>
   );
 }
 
