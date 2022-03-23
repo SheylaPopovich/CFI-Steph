48b13ad0545958f941bb2bbca2b9cdcf8339bed9
Sheyla <Sheylapopovich@gmail.com>
2022-03-22 19:54:24 -0600

    created LandingPage function in component file

--- cfi-steph/src/components/LandingPage/LandingPage.js
+++ cfi-steph/src/components/LandingPage/LandingPage.js
@@ -1,1 +1,11 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
+// import Homepage from "../components/HomePage/Homepage";
+
+function LandingPage() {
+  return (
+    <Button variant="btn btn-success" onClick="/#contact" href="/contact">
+      Click button to view products
+    </Button>
+  );
+}
