80e972757835093d24570121aefac32f1ecd93f4
Sheyla <Sheylapopovich@gmail.com>
2022-04-17 08:42:28 -0600

    added min-height to the body of landing pg css

--- cfi-steph/src/App.js
+++ cfi-steph/src/App.js
@@ -1,37 +1,23 @@
 import "./App.css";
 import Nav from "./components/NavBar/Nav";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 // import Main from "./pages/Main";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
 // import Homepage from "./components/HomePage/Homepage";
 
-
-
 function App() {
   return (
     <div className="App">
-      
-   
-    <Router>
+      <Router>
         <>
-        
-      <Nav />
+          <Nav />
           <Routes>
-            <Route exact path="/" element={ <LandingPage /> }>
-
-
-            </Route>
-           
+            <Route exact path="/" element={<LandingPage />}></Route>
           </Routes>
         </>
       </Router>
-    
-    
-     
     </div>
-
-
   );
 }
 
